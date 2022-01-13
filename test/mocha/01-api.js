8692226072bc02dc7ab87f8b2428b84f66beb4e0
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2022-01-12 22:58:47 -0500

    Setup test.

---
+++ test/mocha/01-api.js
@@ -0,0 +1,10 @@
+/*!
+ * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+describe('httpClientHandler', () => {
+  it('runs test', () => {
+    console.log('test ran successfully');
+  });
+});
---
+++ test/test.js
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
+ */
+const bedrock = require('bedrock');
+require('bedrock-express');
+
+require('bedrock-test');
+bedrock.start();
