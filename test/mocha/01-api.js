1c48833075efe928a928c34008f021af729acfee
Dave Longley <dlongley@digitalbazaar.com>
2026-05-24 21:52:46 -0400

    Pin to `@fastify/express@4.0.5`.

--- test/mocha/01-api.js
+++ test/mocha/01-api.js
@@ -33,7 +33,7 @@ describe('configured routes', () => {
     } catch(e) {
       err = e;
     }
-    should.not.exist(err);
+    assertNoError(err);
     should.exist(res);
     res.status.should.equal(200);
     res.data.success.should.equal(true);
